9aef61100916f272628a9b1b14b4677180ce7b66
CristianC707 <cristian.e.cuevas707@gmail.com>
2022-05-09 19:42:44 -0400

    added feature to delete inputs after each submit.

--- script.js
+++ script.js
@@ -2,24 +2,38 @@ const gradeEntryForm = document.getElementById("grade-entry-form");
 const gradesTable = document.getElementById('grades-table')
 
 const grades = [
-    {firstName: 'Anita', lastName: 'Hero', grade: 88},
-    {firstName: 'Daya', lastName: 'Bolical', grade: 90},
-    {firstName: 'Mike', lastName: 'Wisoski', grade: 99}
+    // {firstName: 'Anita', lastName: 'Hero', grade: 88},
+    // {firstName: 'Daya', lastName: 'Bolical', grade: 90},
+    // {firstName: 'Mike', lastName: 'Wisoski', grade: 99}
 ]
 
-grades.forEach((grade)=>{
-    const tableRow = document.createElement('tr')
-    tableRow.classList.add('rowstyle')
-    tableRow.innerHTML= `<td class="cellstyle">${grade.firstName}</td>
-    <td>${grade.lastName}</td>
-    <td>${grade.grade}</td>`
-    gradesTable.appendChild(tableRow)
-})
+function renderGradesTable() {
+    gradesTable.innerHTML = '<tr> <th>First Name</th> <th>Last Name</th> <th>Grade</th> </tr>'
+    grades.forEach((grade) => {
+        const tableRow = document.createElement('tr')
+        tableRow.classList.add('rowstyle')
+        tableRow.innerHTML = `<td class="cellstyle">${grade.firstName}</td>
+        <td>${grade.lastName}</td>
+        <td>${grade.grade}</td>`
+        gradesTable.appendChild(tableRow)
+    });
+}
 
 gradeEntryForm.addEventListener('submit', function (e) {
-   e.preventDefault()
-   
-    console.log(e.target[0].value);
+    e.preventDefault()
+
+    grades.push({
+        firstName: e.target[0].value, lastName: e.target[1].value,
+        grade: e.target[2].value
+    });
+
+    for (let i=0; i<3; i++){
+        e.target[i].value = ""
+    }
+
+    renderGradesTable();
+
 });
+
 
 
