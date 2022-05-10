3bbbb360b705fcb7080a4248338fff37d9f24916
CristianC707 <cristian.e.cuevas707@gmail.com>
2022-05-09 20:14:36 -0400

    Added function to add letter grade

--- script.js
+++ script.js
@@ -2,38 +2,72 @@ const gradeEntryForm = document.getElementById("grade-entry-form");
 const gradesTable = document.getElementById('grades-table')
 
 const grades = [
-    // {firstName: 'Anita', lastName: 'Hero', grade: 88},
-    // {firstName: 'Daya', lastName: 'Bolical', grade: 90},
-    // {firstName: 'Mike', lastName: 'Wisoski', grade: 99}
+
 ]
+renderGradesTable();
 
 function renderGradesTable() {
-    gradesTable.innerHTML = '<tr> <th>First Name</th> <th>Last Name</th> <th>Grade</th> </tr>'
+    gradesTable.innerHTML = '<tr> <th>First Name</th> <th>Last Name</th> <th>Grade</th> <th>Letter</th> </tr>'
     grades.forEach((grade) => {
         const tableRow = document.createElement('tr')
         tableRow.classList.add('rowstyle')
         tableRow.innerHTML = `<td class="cellstyle">${grade.firstName}</td>
         <td>${grade.lastName}</td>
-        <td>${grade.grade}</td>`
+        <td>${grade.grade}</td>
+        <td>${grade.letter}</td>`
         gradesTable.appendChild(tableRow)
     });
+}
+
+function letterGrade(grade) {
+    const gradeNumber = parseInt(grade)
+    console.log(grade)
+
+    if(grade >=90){
+        return 'A'
+    } else if (grade >= 80){
+        return 'B'
+    } else if (grade >= 70){
+        return 'C'
+    } else if (grade >= 60){
+        return 'D'
+    } 
+    else return 'F'
 }
 
 gradeEntryForm.addEventListener('submit', function (e) {
     e.preventDefault()
 
+
+
     grades.push({
         firstName: e.target[0].value, lastName: e.target[1].value,
-        grade: e.target[2].value
+        grade: e.target[2].value, letter: letterGrade(e.target[2].value)
     });
 
-    for (let i=0; i<3; i++){
+    grades.sort(function (x, y) {
+        return y.grade - x.grade;
+    });
+
+    for (let i = 0; i < 3; i++) {
         e.target[i].value = ""
     }
 
     renderGradesTable();
 
+
+
+
 });
 
+// ASSIGNMENT 1 ============
+// Show both number and grade
+// Color row based on letter
 
+// ASSIGNMENT 2 ===========
+// Add validation
+// Add local storage
+// Add id's for delete function
+// Add delete function 
+// Cant add same student twice
 
