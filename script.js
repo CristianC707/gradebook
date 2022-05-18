a3370aa671733e605ddef442244399038d983e20
CristianC707 <cristian.e.cuevas707@gmail.com>
2022-05-18 19:50:24 -0400

    Added validation for our form

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ renderGradesTable();
 
 function renderGradesTable() {
     gradesTable.innerHTML = '<tr> <th>First Name</th> <th>Last Name</th> <th>Grade</th> <th>Letter</th> </tr>'
-    
+
     grades.forEach((grade) => {
         const tableRow = document.createElement('tr')
-        tableRow.classList.add('rowstyle')
+        tableRow.classList.add(getRowColor(grade.letter));
         tableRow.innerHTML = `<td class="cellstyle">${grade.firstName}</td>
         <td>${grade.lastName}</td>
         <td>${grade.grade}</td>
@@ -20,51 +20,93 @@ function renderGradesTable() {
     });
 }
 
+function getRowColor(letter) {
+    switch (letter) {
+        case "A":
+            return "green-row";
+
+        case "B":
+            return "yellow-row";
+
+        case "C":
+            return "blue-row";
+
+        case "D":
+            return "orange-row";
+
+        case "F":
+            return "red-row";
+    };
+};
+
 function letterGrade(grade) {
-    if(grade >=90){
+    if (grade >= 90) {
         return 'A'
-    } else if (grade >= 80){
+    } else if (grade >= 80) {
         return 'B'
-    } else if (grade >= 70){
+    } else if (grade >= 70) {
         return 'C'
-    } else if (grade >= 60){
+    } else if (grade >= 60) {
         return 'D'
-    } 
+    }
     else return 'F'
 }
 
 gradeEntryForm.addEventListener('submit', function (e) {
     e.preventDefault()
+    // hide all error messages
+    document.getElementById('blankError').classList.add('hide');
+    document.getElementById('numError').classList.add('hide');
 
-    grades.push({
+
+    const grade = {
         firstName: e.target[0].value, lastName: e.target[1].value,
         grade: e.target[2].value, letter: letterGrade(e.target[2].value)
-    });
+    }
+
+    const valid = validate(grade)
+    if (valid) {
+
+        addGrade(grade)
+
+        for (let i = 0; i < 3; i++) {
+            e.target[i].value = ""
+        }
+    }
+
+});
+
+function validate(grade) {
+    //return true or false
+    if (!grade.firstName || !grade.lastName || !grade.grade) {
+        document.getElementById('blankError').classList.remove('hide');
+        return false;
+    }
+    else if (isNaN(grade.grade)) {
+        document.getElementById('numError').classList.remove('hide')
+    }
+    else {
+        return true;
+    }
+}
+
+function addGrade(grade) {
+    grades.push(grade);
 
     grades.sort(function (x, y) {
         return y.grade - x.grade;
     });
 
-    localStorage.setItem("grades", JSON.stringify(grades) )
+    localStorage.setItem("grades", JSON.stringify(grades))
 
-    for (let i = 0; i < 3; i++) {
-        e.target[i].value = ""
-    }
 
     renderGradesTable();
+}
 
-
-
-
-});
-
-// ASSIGNMENT 1 ============
-// Color row based on letter
 
 // ASSIGNMENT 2 ===========
-// Add validation
 // Add id's for delete function
-// Add delete function 
+// Add delete function
 // Cant add same student twice
 // Add to github pages
 
